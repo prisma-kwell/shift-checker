23e7aba17d6f8f0e6c33a3aed841d6e8241123a8
Jan <lepetitjan@users.noreply.github.com>
2017-01-23 02:46:58 +0100

    Update checkdelegate.php

--- checkdelegate.php
+++ checkdelegate.php
@@ -154,7 +154,7 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
    			}
 
        	passthru("cd $pathtoapp && forever stop app.js");
-       	passthru("cd $snapshotDir && echo y | bash shift-snapshot.sh restore");
+       	passthru("cd $snapshotDir && echo y | ./shift-snapshot.sh restore");
        	passthru("cd $pathtoapp && forever start app.js");
 
         echo $date." - [ FORKING ] Finally, I will reset the counter for you...\n";
@@ -206,7 +206,7 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
 				echo $date." - [ SNAPSHOT ] No snapshot exists for today, I will create one for you now!\n";
 					
 				ob_start();
-				$create = passthru("cd $snapshotDir && bash shift-snapshot.sh create");
+				$create = passthru("cd $snapshotDir && ./shift-snapshot.sh create");
 				$check_createoutput = ob_get_contents();
 				ob_end_clean();
 

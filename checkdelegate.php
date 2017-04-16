fa7a74997785de6853f2c858f82f65c2dedcd2b0
lepetitjan <jeeweevee@live.nl>
2017-04-16 22:44:06 +0200

    Fixed some relative paths

--- checkdelegate.php
+++ checkdelegate.php
@@ -7,8 +7,8 @@
 
 /* PREREQUISITES
 ____________________ */
-require('./config.php');
-require('./functions.php');
+require(dirname(__FILE__).'/config.php');
+require(dirname(__FILE__).'/functions.php');
 
 // Let's start the output with a line for the log file
 echo "___________________________________________________\n";

83fed3ef0926ba83c87dbc3ba5e2173ecf47b581
lepetitjan <jeeweevee@live.nl>
2017-04-17 02:40:21 +0200

    Adjusted indentation

--- checkdelegate.php
+++ checkdelegate.php
@@ -193,9 +193,12 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
 
     }
 
-  // Consensus functionality
-  echo $date." - [ CONSENSUS ] Checking if you enabled the consensus check...";
-  if($consensusEnable === true){
+/* CHECK CONSENSUS
+____________________ */
+echo $date." - [ CONSENSUS ]\n";
+
+  echo "\t\t\tChecking if you enabled the consensus check...";
+  if($consensusEnable === true && !empty($secret)){
     echo "yes!\n";
 
     // Get publicKey of the first secret to use in forging checks
@@ -204,10 +207,10 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
     // Check if we are the master node
     if($master === false){
       // If we land here, we are the slave
-      echo $date." - [ CONSENSUS ] We are a slave\n";
+      echo "\t\t\tWe are a slave\n";
       
       // Check if the master is online
-      echo $date." - [ CONSENSUS ] Checking if master is online...";
+      echo "\t\t\tChecking if master is online...";
       
       $find = array("http://","https://");
       $up = ping(str_replace($find,"",$masternode), $masterport);
@@ -216,7 +219,7 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
         echo "yes!\n";
 
         // Check if we are forging
-        echo $date." - [ CONSENSUS ] Checking if we (slave) are forging...";
+        echo "\t\t\tChecking if we (slave) are forging...";
         $forging = checkForging($slavenode.":".$slaveport, $public);
         
         // If we are forging..
@@ -229,13 +232,13 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
       }else{
         // Master is offline. Let's check if we are forging, if not; enable it. 
         echo "no!\n";
-        echo $date." - [ CONSENSUS ] Let's check if we (slave) are forging...\n";
+        echo "\t\t\tLet's check if we (slave) are forging...\n";
         $forging = checkForging($slavenode.":".$slaveport, $public);
         
         // If we are forging..
         if($forging == "true"){
           echo "yes!\n";
-          echo $date." - [ CONSENSUS ] Checking our consensus..\n";
+          echo "\t\t\tChecking our consensus..\n";
 
           // Perform a consensus check..
           // Check consensus on slave node
@@ -246,11 +249,11 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
             $consensusSlave = json_decode($consensusSlave, true);
             $consensusSlave = $consensusSlave['consensus'];
           }
-          echo $date." - [ CONSENSUS ] Consensus slave: $consensusSlave %\n";
+          echo "\t\t\tConsensus slave: $consensusSlave %\n";
           
           // If consensus on the slave is below threshold as well, send a telegram message and restart Shift!
           if($consensusSlave <= $threshold){
-            echo $date." - [ CONSENSUS ] Threshold on slave node reached! Telegram: No healthy server online. Going to restart Shift for you..\n";
+            echo "\t\t\tThreshold on slave node reached! Telegram: No healthy server online. Going to restart Shift for you..\n";
             
             // Send Telegram
             if($telegramEnable === true){
@@ -259,40 +262,40 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
             }
 
             // Restart Shift
-            echo $date." - [ CONSENSUS ] Stopping all forever processes...\n";
+            echo "\t\t\tStopping all forever processes...\n";
               passthru("forever stopall");
-            echo $date." - [ CONSENSUS ] Starting Shift forever proces...\n";
+            echo "\t\t\tStarting Shift forever proces...\n";
               passthru("cd $pathtoapp && forever start app.js");
               
           }else{
             // All is fine. Do nothing..
-            echo $date." - [ CONSENSUS ] Consensus is fine!\n";
+            echo "\t\t\tConsensus is fine!\n";
           }
         }else{
           // Enable forging for each secret on the slave
-          echo $date." - [ CONSENSUS ] We are not forging! Let's enable it..\n";
+          echo "\t\t\tWe are not forging! Let's enable it..\n";
           
           foreach($secret as $sec){
-            echo $date." - [ CONSENSUS ] Enabling forging on slave for secret: $sec\n";
+            echo "\t\t\tEnabling forging on slave for secret: $sec\n";
             enableForging($slavenode.":".$slaveport, $sec);
           }
         }
       }
     }else{
       // If we land here, we are the master
-      echo $date." - [ CONSENSUS ] We are the master\n";
+      echo "\t\t\tWe are the master\n";
         
       // Check if we are forging
       $forging = checkForging($masternode.":".$masterport, $public);
 
       // If we are forging..
       if($forging == "true"){
-        echo $date." - [ CONSENSUS ] Master node is forging.\n";
+        echo "\t\t\tMaster node is forging.\n";
 
         // Forging on the slave should be/stay disabled for every secret until we perform a consensus check.
         // This way we ensure that forging is only disabled on nodes the master chooses.
         foreach($secret as $sec){
-          echo $date." - [ CONSENSUS ] Disabling forging on slave for secret: $sec\n";
+          echo "\t\t\tDisabling forging on slave for secret: $sec\n";
           disableForging($slavenode.":".$slaveport, $sec);
         }
 
@@ -304,11 +307,11 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
           $consensusMaster = json_decode($consensusMaster, true);
           $consensusMaster = $consensusMaster['consensus'];
         }
-        echo $date." - [ CONSENSUS ] Consensus master: $consensusMaster %\n";
+        echo "\t\t\tConsensus master: $consensusMaster %\n";
         
         // If consensus is the same as or lower than the set threshold..
         if($consensusMaster <= $threshold){
-          echo $date." - [ CONSENSUS ] Threshold on master node reached! Going to check the slave node..\n";
+          echo "\t\t\tThreshold on master node reached! Going to check the slave node..\n";
         
           // Check consensus on slave node
           $consensusSlave = @file_get_contents($slavenode.":".$slaveport."/api/loader/status/sync");
@@ -318,11 +321,11 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
             $consensusSlave = json_decode($consensusSlave, true);
             $consensusSlave = $consensusSlave['consensus'];
           }
-          echo $date." - [ CONSENSUS ] Consensus slave: $consensusSlave %\n";
+          echo "\t\t\tConsensus slave: $consensusSlave %\n";
           
           // If consensus on the slave is below threshold as well, send a telegram message and restart Shift!
           if($consensusSlave <= $threshold){
-            echo $date." - [ CONSENSUS ] Threshold on slave node reached! Telegram: No healthy server online. Going to restart Shift for you..\n";
+            echo "\t\t\tThreshold on slave node reached! Telegram: No healthy server online. Going to restart Shift for you..\n";
             
             // Send Telegram
             if($telegramEnable === true){
@@ -331,39 +334,39 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
             }
 
             // Restart Shift
-            echo $date." - [ CONSENSUS ] Stopping all forever processes...\n";
+            echo "\t\t\tStopping all forever processes...\n";
               passthru("forever stopall");
-            echo $date." - [ CONSENSUS ] Starting Shift forever proces...\n";
+            echo "\t\t\tStarting Shift forever proces...\n";
               passthru("cd $pathtoapp && forever start app.js");
           }else{
-            echo $date." - [ CONSENSUS ] Consensus on slave is sufficient enough to switch to..\n";
+            echo "\t\t\tConsensus on slave is sufficient enough to switch to..\n";
             
-            echo $date." - [ CONSENSUS ] Enabling forging on slave..\n";
+            echo "\t\t\tEnabling forging on slave..\n";
             foreach($secret as $sec){
-              echo $date." - [ CONSENSUS ] Enabling forging on slave for secret: $sec\n";
+              echo "\t\t\tEnabling forging on slave for secret: $sec\n";
               enableForging($slavenode.":".$slaveport, $sec);
             }
 
-            echo $date." - [ CONSENSUS ] Disabling forging on master..\n";
+            echo "\t\t\tDisabling forging on master..\n";
             foreach($secret as $sec){
-              echo $date." - [ CONSENSUS ] Disabling forging on master for secret: $sec\n";
+              echo "\t\t\tDisabling forging on master for secret: $sec\n";
               disableForging($masternode.":".$masterport, $sec);
             }
           }
         }else{
           // Master consensus is high enough to continue forging
-          echo $date." - [ CONSENSUS ] Threshold on master node not reached. Everything is okay.\n";
+          echo "\t\t\tThreshold on master node not reached. Everything is okay.\n";
         }
 
       // If we are not forging..
       }else{
-        echo $date." - [ CONSENSUS ] Master node is not forging. Checking if slave is forging..\n";
+        echo "\t\t\tMaster node is not forging. Checking if slave is forging..\n";
 
         // Check if the slave is forging
         $forging = checkForging($slavenode.":".$slaveport, $public);
         // If slave is forging..
         if($forging == "true"){
-          echo $date." - [ CONSENSUS ] Slave is forging. Going to check it's consensus..\n";
+          echo "\t\t\tSlave is forging. Going to check it's consensus..\n";
 
           // Check consensus on slave node
           $consensusSlave = @file_get_contents($slavenode.":".$slaveport."/api/loader/status/sync");
@@ -373,11 +376,11 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
             $consensusSlave = json_decode($consensusSlave, true);
             $consensusSlave = $consensusSlave['consensus'];
           }
-          echo $date." - [ CONSENSUS ] Consensus slave: $consensusSlave %\n";
+          echo "\t\t\tConsensus slave: $consensusSlave %\n";
 
           // If consensus is the same as or lower than the set threshold..
           if($consensusSlave <= $threshold){
-            echo $date." - [ CONSENSUS ] Consensus slave reached the threshold. Checking consensus master node..\n";
+            echo "\t\t\tConsensus slave reached the threshold. Checking consensus master node..\n";
 
             // Check consensus on master node
             $consensusMaster = @file_get_contents($masternode.":".$masterport."/api/loader/status/sync");
@@ -387,33 +390,33 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
               $consensusMaster = json_decode($consensusMaster, true);
               $consensusMaster = $consensusMaster['consensus'];
             }
-            echo $date." - [ CONSENSUS ] Consensus master: $consensusMaster %\n";
+            echo "\t\t\tConsensus master: $consensusMaster %\n";
             
             // If consensus is the same as or lower than the set threshold..
             if($consensus <= $threshold){
-              echo $date." - [ CONSENSUS ] Threshold on master node reached as well! Restarting Shift..\n";
+              echo "\t\t\tThreshold on master node reached as well! Restarting Shift..\n";
 
               // Restart Shift
-              echo $date." - [ CONSENSUS ] Stopping all forever processes...\n";
+              echo "\t\t\tStopping all forever processes...\n";
                 passthru("forever stopall");
-              echo $date." - [ CONSENSUS ] Starting Shift forever proces...\n";
+              echo "\t\t\tStarting Shift forever proces...\n";
                 passthru("cd $pathtoapp && forever start app.js");
 
             }else{
               // Consensus is sufficient on master. Enabling forging to master
-              echo $date." - [ CONSENSUS ] Consensus on master is sufficient. Enabling forging on master..\n";
+              echo "\t\t\tConsensus on master is sufficient. Enabling forging on master..\n";
 
               // Enable forging on master
-              echo $date." - [ CONSENSUS ] Enabling forging on master..\n";
+              echo "\t\t\tEnabling forging on master..\n";
               foreach($secret as $sec){
-                echo $date." - [ CONSENSUS ] Enabling forging on master for secret: $sec\n";
+                echo "\t\t\tEnabling forging on master for secret: $sec\n";
                 enableForging($masternode.":".$masterport, $sec);
               }
 
               // Disable forging on slave
-              echo $date." - [ CONSENSUS ] Disabling forging on slave..\n";
+              echo "\t\t\tDisabling forging on slave..\n";
               foreach($secret as $sec){
-                echo $date." - [ CONSENSUS ] Disabling forging on slave for secret: $sec\n";
+                echo "\t\t\tDisabling forging on slave for secret: $sec\n";
                 disableForging($slavenode.":".$slaveport, $sec);
               }
 
@@ -421,11 +424,11 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
 
           }else{
             // Consensus slave is sufficient. Doing nothing..
-            echo $date." - [ CONSENSUS ] Consensus on slave is sufficient. Doing nothing..\n";
+            echo "\t\t\tConsensus on slave is sufficient. Doing nothing..\n";
           }
         }else{
           // Slave is also not forging! Compare consensus on both nodes and enable forging on node with highest consensus..
-          echo $date." - [ CONSENSUS ] Slave is not forging as well! Going to compare consensus and enable forging on best node..\n";
+          echo "\t\t\tSlave is not forging as well! Going to compare consensus and enable forging on best node..\n";
 
           // Check consensus on master node
           $consensusMaster = @file_get_contents($masternode.":".$masterport."/api/loader/status/sync");
@@ -435,7 +438,7 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
             $consensusMaster = json_decode($consensusMaster, true);
             $consensusMaster = $consensusMaster['consensus'];
           }
-          echo $date." - [ CONSENSUS ] Consensus master: $consensusMaster %\n";
+          echo "\t\t\tConsensus master: $consensusMaster %\n";
 
           // Check consensus on slave node
           $consensusSlave = @file_get_contents($slavenode.":".$slaveport."/api/loader/status/sync");
@@ -445,19 +448,19 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
             $consensusSlave = json_decode($consensusSlave, true);
             $consensusSlave = $consensusSlave['consensus'];
           }
-          echo $date." - [ CONSENSUS ] Consensus slave: $consensusSlave %\n";
+          echo "\t\t\tConsensus slave: $consensusSlave %\n";
 
           // COMPARE CONSENSUS
           if($consensusMaster > $consensusSlave){
             // Enable forging on master
             foreach($secret as $sec){
-              echo $date." - [ CONSENSUS ] Enabling forging on master for secret: $sec\n";
+              echo "\t\t\tEnabling forging on master for secret: $sec\n";
               enableForging($masternode.":".$masterport, $sec);
             }
           }else{
             // Enabling forging on slave
             foreach($secret as $sec){
-              echo $date." - [ CONSENSUS ] Enabling forging on slave for secret: $sec\n";
+              echo "\t\t\tEnabling forging on slave for secret: $sec\n";
               enableForging($slavenode.":".$slaveport, $sec);
             }
           } // END: COMPARE CONSENSUS
@@ -469,7 +472,7 @@ echo $date." - [ FORKING ] Going to check for forked status now...\n";
     } // END: WE ARE THE MASTER
 
   }else{
-    echo "no!\n";
+    echo "no! (or no secret)\n";
   } // END: ENABLED CONSENSUS CHECK?
 
 // Cleaning up your log file(s)
